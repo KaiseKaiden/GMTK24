83db73d102623da639b2730745eebb24f94df69e
Alexander Frost <LexFrost01@hotmail.com>
2024-08-17 23:40:12 +0200

    skybox

--- Unity Project/Assets/People Test/Tomas/Shadersandmats/GlobalShaderVariables.cs
+++ Unity Project/Assets/People Test/Tomas/Shadersandmats/GlobalShaderVariables.cs
@@ -7,7 +7,7 @@ public class GlobalShaderVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Shader.SetGlobalFloat("_AFloat", 1.0");
+        Shader.SetGlobalFloat("_AFloat", 1.0f);
     }
 
     // Update is called once per frame
