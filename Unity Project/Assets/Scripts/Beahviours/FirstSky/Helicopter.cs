fb5db1fa3def9bcb9e2f326c8e89e0bfc9f5eec0
Alexander Frost <LexFrost01@hotmail.com>
2024-08-19 00:30:07 +0200

    Helicopter

---
+++ Unity Project/Assets/Scripts/Beahviours/FirstSky/Helicopter.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : Behaviour
+{
+    float myTime;
+    Vector3 myStartPosition;
+    Vector3 myLastPosition;
+
+    [SerializeField] float myMapWidth;
+    [SerializeField] float myAngularTilt;
+    [SerializeField] TrailRenderer[] myTrails;
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myStartPosition = transform.position;
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public override void Move()
+    {
+        myTime += Time.deltaTime * 0.25f;
+
+        float dir = Mathf.Sin(myTime);
+
+        Vector3 position = new Vector3(dir * Mathf.Clamp01(myTime) * myMapWidth, myStartPosition.y, 0.0f);
+        position.z = GameManager.Instance.GetZFromY(position.y);
+        transform.position = position;
+
+        Vector3 direction = (position - myLastPosition);
+        direction.z = 0.0f;
+        direction.Normalize();
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), 5.0f * Time.deltaTime);
+        transform.eulerAngles = new Vector3(Mathf.Sin(myTime * 10.0f) * -myAngularTilt, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        myLastPosition = position;
+    }
+
+    public override void Picked()
+    {
+        myIsBeingPicked = true;
+
+        myStartPosition = transform.position;
+        myLastPosition = transform.position;
+
+        foreach (TrailRenderer t in myTrails)
+        {
+            t.emitting = false;
+        }
+    }
+
+    public override void Dropped()
+    {
+        myIsBeingPicked = false;
+
+        myStartPosition = transform.position;
+        myLastPosition = transform.position;
+
+        myTime = 0.0f;
+        myRigidbody.useGravity = false;
+
+        foreach (TrailRenderer t in myTrails)
+        {
+            t.emitting = true;
+        }
+    }
+}
