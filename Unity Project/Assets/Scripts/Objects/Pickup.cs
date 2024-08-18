a21c9eb63e16fcd53e35740958d4f3b58d36bad5
Lukas Ternelius <lukas.ternelius@gmail.com>
2024-08-18 18:18:48 +0200

    nestUpdates

--- Unity Project/Assets/Scripts/Objects/Pickup.cs
+++ Unity Project/Assets/Scripts/Objects/Pickup.cs
@@ -1,15 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] Transform myPickupPoint;
+    [SerializeField]
+    Transform myPickupPoint;
     PlayerMovement myPlayerMovement;
     Rigidbody myRigidbody;
 
-    [SerializeField] int myNestCapacity;
-    [SerializeField] int myLevelRequred;
+    [SerializeField]
+    int myNestCapacity;
+    [SerializeField]
+    int myLevelRequred;
 
     bool myIsDragging;
     float myDraggingLerpTime;
@@ -26,7 +28,21 @@ public class Pickup : MonoBehaviour
 
         myRigidbody = GetComponent<Rigidbody>();
     }
+    public IEnumerator MoveTowardPoint(Vector3 point)
+    {
+        myRigidbody.useGravity = false;
+        myIsDragging = false;
+        myRigidbody.isKinematic = true;
+        GetComponent<Pickup>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
 
+        while ((point - transform.position).magnitude > .5f)
+        {
+            transform.Translate(Time.deltaTime * (point - transform.position));
+            yield return null;
+        }
+        NestCreator.myNestCreator.Increment();
+    }
     private void Update()
     {
         if (myIsDragging)
@@ -34,15 +50,17 @@ public class Pickup : MonoBehaviour
             myPerlinX += Time.deltaTime;
             myPerlinY += Time.deltaTime;
 
-            transform.eulerAngles = new Vector3(Mathf.PerlinNoise(myPerlinX, 0.0f) * 50.0f, Mathf.PerlinNoise(0.0f, myPerlinY) * 50.0f, 0.0f);
+            transform.eulerAngles = new Vector3(Mathf.PerlinNoise(myPerlinX, 0.0f) * 50.0f,
+                                                Mathf.PerlinNoise(0.0f, myPerlinY) * 50.0f, 0.0f);
             transform.eulerAngles = transform.eulerAngles + new Vector3(-myPlayerMovement.GetVelocity().x, 0.0f, 0.0f);
 
             Vector3 diffrence = (myPlayerMovement.GetRightLeg().position - myPickupPoint.position);
 
-
             myDraggingLerpTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(myStartPosition, transform.position + diffrence, EaseOutCirc(Mathf.Clamp01(myDraggingLerpTime)));
-            transform.rotation = Quaternion.Lerp(myStartRotation, transform.rotation, EaseOutCirc(Mathf.Clamp01(myDraggingLerpTime)));
+            transform.position = Vector3.Lerp(myStartPosition, transform.position + diffrence,
+                                              EaseOutCirc(Mathf.Clamp01(myDraggingLerpTime)));
+            transform.rotation =
+                Quaternion.Lerp(myStartRotation, transform.rotation, EaseOutCirc(Mathf.Clamp01(myDraggingLerpTime)));
         }
         else
         {
