2b52c9d2c139a286599d37ad1ad57529aecebc27
Alexander Frost <LexFrost01@hotmail.com>
2024-08-20 16:01:19 +0200

    menu polish

--- Unity Project/Assets/Scripts/Menus/GeeserFeed.cs
+++ Unity Project/Assets/Scripts/Menus/GeeserFeed.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GeeserFeed : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] ParticleSystem myFoodParticle;
 
-    // Update is called once per frame
-    void Update()
+    public void Feed()
     {
-        
+        myFoodParticle.Play();
     }
 }
