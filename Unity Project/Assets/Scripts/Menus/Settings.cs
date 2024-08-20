32ce0162f4fb1cd7810febf6abc2e51ccba19e4f
Jophan <03jophan@gmail.com>
2024-08-20 17:53:10 +0200

    Settings

--- Unity Project/Assets/Scripts/Menus/Settings.cs
+++ Unity Project/Assets/Scripts/Menus/Settings.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    public void SetSoundLevel(float aLevel01)
+    public void SetSoundLevel(GameObject aSoundSlider)
     {
-
+        float value = aSoundSlider.GetComponent<Slider>().value * 100f;
+        AudioManager.instance.SetVolume(value);
     }
 
-    public void SetMusicLevel(float aLevel01)
+    public void SetMusicLevel(GameObject aMusicSlider)
     {
+        float value = aMusicSlider.GetComponent<Slider>().value * 100f;
+        AudioManager.instance.SetMusicVolume(value);
+    }
 
+    public void SetFullscreen(GameObject aFullscreenToggle)
+    {
+        bool value = aFullscreenToggle.GetComponent<Toggle>().isOn;
+        Screen.fullScreen = value;
     }
 }
