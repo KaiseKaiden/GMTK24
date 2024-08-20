0d3d9d5a5e4593ffbfa49d49eef69352317c4dc2
Jophan <03jophan@gmail.com>
2024-08-20 14:50:08 +0200

    UI stuff

--- Unity Project/Assets/Scripts/Menus/PauseMenu.cs
+++ Unity Project/Assets/Scripts/Menus/PauseMenu.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject myPauseMenu;
+    public GameObject myCreditsMenu;
+    public GameObject mySettingsMenu;
+    public GameObject myLeaderboardMenu;
+
+    private void Start()
     {
-        
+        if (myPauseMenu.activeSelf)
+        {
+            myPauseMenu.SetActive(false);
+        }
+    }
+    public void Settings()
+    {
+        AudioManager.instance.PlayOneshotNoLocation(FMODEvents.instance.SettingsButtonEvent);
+        mySettingsMenu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Leaderboard()
     {
-        
+        AudioManager.instance.PlayOneshotNoLocation(FMODEvents.instance.LeaderBoardButtonEvent);
+        myLeaderboardMenu.SetActive(true);
     }
+
+    public void Credits()
+    {
+        AudioManager.instance.PlayOneshotNoLocation(FMODEvents.instance.CreditButtonEvent);
+        myCreditsMenu.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        AudioManager.instance.PlayOneshotNoLocation(FMODEvents.instance.StartMainButtonEvent);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Continue()
+    {
+        AudioManager.instance.PlayOneshotNoLocation(FMODEvents.instance.BirdWingFlapEvent);
+        myPauseMenu.SetActive(false);
+    }
+
+    public void CloseMenu(string aMenuName)
+    {
+        if (aMenuName == "Credits")
+        {
+            myCreditsMenu.SetActive(false);
+        }
+        else if (aMenuName == "Leaderboard")
+        {
+            myLeaderboardMenu.SetActive(false);
+        }
+        else if (aMenuName == "Settings")
+        {
+            mySettingsMenu.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Invalid Menu Name");
+        }
+    }
+
 }
